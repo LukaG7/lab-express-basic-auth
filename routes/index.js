76bcad420b837936c0ccbb5cbca469eea3ba93c6
Lukas Gonzalez <lukegonz47@gmail.com>
2022-09-23 15:13:33 -0400

    iteration 1 complete

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 const router = require("express").Router();
+const bcryptjs = require('bcryptjs');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup.hbs')
+  const myEmail = req.body.email;
+  const myPassword = req.body.password;
+
+  const myHashedPassword = bcryptjs.hashSync(myPassword)
+
+  User.create({
+    username: myUsername,
+    password: myHashedPassword
+  })
+  .then(savedUser => {
+    console.log(savedUser);
+    res.redirect('/login')
+  })
+  .catch(err => {
+    console.log(err)
+    res.send(err);
+  })
 });
 
 module.exports = router;
